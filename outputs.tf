35c0c27fd0a7e52a87f4d3f28eca4096e4537815
alexander.miehe <alexander.miehe@flaconi.de>
2024-01-18 13:00:37 +0100

    PLT-792 - Add support for cognito authorizer
    * allow to create a cognito authorizer

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "root_resource_id" {
   description = "The resource ID of the REST API's root"
   value       = var.enabled ? aws_api_gateway_rest_api.this[0].root_resource_id : null
 }
+
+output "cognito_authorizer_id" {
+  description = "The authorizer ID of the REST API's cognito authorizer"
+  value       = local.create_cognito_authorizer ? aws_api_gateway_authorizer.cognito[0].id : null
+}
